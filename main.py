e1e4f3797055cfeee93b9855610b449a3915302a
Max T <maxtrebula@gmail.com>
2020-10-22 08:12:57 +0200

    pygame initialized

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+# Hra 1
+# https://realpython.com/pygame-a-primer/
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
 
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+# Run until the user asks to quit
+running = True
+while running:
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    # Fill the background with white
+    screen.fill((255, 255, 255))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
